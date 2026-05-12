d9b369c9d97603c7e349b4ebf7c30bcf2eadaf38
rnewdetailing <r.new.detailing@gmail.com>
2026-05-12 11:19:42 +0300

    Add files via upload

---
+++ servicii_exterior.js
@@ -0,0 +1,212 @@
+ const data_exterior = [
+  {
+    serviciu: "Prespălare",
+    ore_lucru: "0.25",
+    consumabile: 20,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Decontaminare chimică",
+    ore_lucru: "0,35",
+    consumabile: 40,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare cu presiune înaltă",
+    ore_lucru: "0.25",
+    consumabile: 25,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare caroserie și ornamente (standard)",
+    ore_lucru: "0,35",
+    consumabile: 25,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare caroserie și ornamente (detaliată)",
+    ore_lucru: "0,5",
+    consumabile: 40,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare roți (standard)",
+    ore_lucru: "1",
+    consumabile: 36,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare roți (detaliată)",
+    ore_lucru: "2",
+    consumabile: 60,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Uscare cu prosoape ultra-absorbante (fără urme)",
+    ore_lucru: "0,75",
+    consumabile: 12,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Decontaminare mecanică suprafețe vitrate (cu argilă) ",
+    ore_lucru: "1",
+    consumabile: 10,
+    multiplicabil: "NU"
+  },
+  {
+    serviciu: "Decontaminare mecanică caroserie (cu argilă)",
+    ore_lucru: "1,5",
+    consumabile: 25,
+    multiplicabil: "NU"
+  },
+  {
+    serviciu: "Lustruire caroserie (One step)",
+    ore_lucru: "6",
+    consumabile: 80,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Lustruire caroserie (3in1)",
+    ore_lucru: "6,5",
+    consumabile: 85,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Corectare lac (2 steps correction)",
+    ore_lucru: "9",
+    consumabile: 120,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Corectare lac (3 steps correction)",
+    ore_lucru: "12",
+    consumabile: 155,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Șlefuire lac (wet/dry sanding/element)",
+    ore_lucru: "2,5",
+    consumabile: 190,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Corectare zgârietură fină",
+    ore_lucru: "0,25",
+    consumabile: 12,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Corectare zgârietură medie",
+    ore_lucru: "1",
+    consumabile: 23,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Corectare zgârietură adâncă",
+    ore_lucru: "1,5",
+    consumabile: 29,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Corectare punct de rugină",
+    ore_lucru: "2",
+    consumabile: 34,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Tratament glaze (extra luciu)",
+    ore_lucru: "0,75",
+    consumabile: 70,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Tratament de protecție caroserie (scurtă durată)",
+    ore_lucru: "0,75",
+    consumabile: 50,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Tratament de protecție caroserie (sealant polimer)",
+    ore_lucru: "1,25",
+    consumabile: 65,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Tratament de protecție caroserie (ceară standard)",
+    ore_lucru: "1,5",
+    consumabile: 70,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Tratament de protecție caroserie (ceară premium)",
+    ore_lucru: "2",
+    consumabile: 85,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Tratament de protecție caroserie (coating ceramic)",
+    ore_lucru: "4",
+    consumabile: 450,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Tratament hidrofobic - suprafețe vitrate",
+    ore_lucru: "1,5",
+    consumabile: 60,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Revitalizare faruri / stopuri",
+    ore_lucru: "1",
+    consumabile: 50,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Restaurare faruri / stopuri (standard)",
+    ore_lucru: "1",
+    consumabile: 65,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Restaurare faruri / stopuri (plus)",
+    ore_lucru: "2",
+    consumabile: 85,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Tratament de protecție cu lac dedicat farurilor",
+    ore_lucru: "1",
+    consumabile: 90,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Revitalizare ornamente din plastic / crom / piano black",
+    ore_lucru: "1",
+    consumabile: 35,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare plafon - soft top (standard)",
+    ore_lucru: "0,7",
+    consumabile: 65,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare plafon- soft top (detaliată)",
+    ore_lucru: "1,5",
+    consumabile: 80,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare compartiment motor (standard)",
+    ore_lucru: "1",
+    consumabile: 30,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare compartiment motor (detaliată)",
+    ore_lucru: "4",
+    consumabile: 75,
+    multiplicabil: "DA"
+  }
+]

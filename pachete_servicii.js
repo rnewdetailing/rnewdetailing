dbf77b6d95d5b2aaf8c33f78405c61e759759f5f
rnewdetailing <r.new.detailing@gmail.com>
2026-05-13 15:55:17 +0300

    Add files via upload

---
+++ pachete_servicii.js
@@ -0,0 +1,56 @@
+  const data_pachete =[
+  {
+    nume_pachet: "Custom",
+    tip: "",
+    servicii_incluse: "",
+    discount_procent: null
+  },
+  {
+    nume_pachet: "Maintenance",
+    tip: "exterior",
+    servicii_incluse: "Prespălare,Decontaminare chimică, Curățare cu presiune înaltă, Curățare roți (standard), Uscare cu prosoape ultra-absorbante (fără urme),Tratament de protecție caroserie (scurtă durată)",
+    discount_procent: 5
+  },
+  {
+    nume_pachet: "Maintenance",
+    tip: "interior",
+    servicii_incluse: "Aspirare mochetă, Aspirare preșuri, Aspirare scaune și banchetă, Aspirare portbagaj, Curățare planșă de bord și consolă centrală (standard), Curățare mânere, butoane, ornamente (standard), Hidratare și protejare suprafețe din plastic / cauciuc",
+    discount_procent: 5
+  },
+  {
+    nume_pachet: "Classic",
+    tip: "exterior",
+    servicii_incluse: "Prespălare, Decontaminare chimică, Curățare cu presiune înaltă, Curățare roți (standard), Uscare cu prosoape ultra-absorbante (fără urme), Decontaminare mecanică caroserie (cu argilă), Lustruire caroserie (One step), Tratament de protecție caroserie (ceară standard), Revitalizare faruri / stopuri",
+    discount_procent: 10
+  },
+  {
+    nume_pachet: "Classic",
+    tip: "interior",
+    servicii_incluse: "Aspirare mochetă, Aspirare preșuri, Aspirare scaune și banchetă, Aspirare portbagaj, Curățare mochetă (standard), Curățare preșuri textile (standard), Curățare scaune și banchetă - textile (standard), Curățare panouri laterale - textile (standard), Curățare planșă de bord și consolă centrală (standard), Curățare mânere / butoane / ornamente (standard), Hidratare și protejare suprafețe din plastic / cauciuc, Curățare compartimente (uși / cotieră / torpedou) (standard), Curățare portbagaj (simplă), Curățare volan - spuma/plastic, Curățare suprafețe vitrate, Curățare pedale",
+    discount_procent: 10
+  },
+  {
+    nume_pachet: "InDepth",
+    tip: "exterior",
+    servicii_incluse: "Prespălare, Decontaminare chimică, Curățare cu presiune înaltă, Curățare caroserie și ornamente (detaliată), Curățare roți (detaliată), Uscare cu prosoape ultra-absorbante (fără urme), Decontaminare mecanică suprafețe vitrate (cu argilă), Decontaminare mecanică caroserie (cu argilă), Corectare lac (2 steps correction), Tratament glaze (extra luciu), Tratament de protecție caroserie (ceară standard), Tratament hidrofobic - suprafețe vitrate, Revitalizare faruri / stopuri, Revitalizare ornamente din plastic / crom / piano black",
+    discount_procent: 12
+  },
+  {
+    nume_pachet: "InDepth",
+    tip: "interior",
+    servicii_incluse: "Aspirare mochetă, Aspirare preșuri, Aspirare scaune și banchetă, Suflare cu aer comprimat a habitaclului, Aspirare portbagaj, Curățare mochetă (standard), Curățare preșuri textile (standard), Curățare scaune și banchetă - textile (detaliată), Curățare panouri laterale - textile (detaliată), Curățare planșă de bord și consolă centrală (detaliată), Curățare mânere / butoane / ornamente (detaliată), Hidratare și protejare suprafețe din plastic / cauciuc, Curățare compartimente (uși / cotieră / torpedou) (detaliată), Curățare portbagaj (detaliată), Curățare volan - spuma/plastic, Curățare plafon textil, Curățare suprafețe vitrate, Curățare pedale",
+    discount_procent: 12
+  },
+  {
+    nume_pachet: "RNEW",
+    tip: "exterior",
+    servicii_incluse: "Prespălare, Decontaminare chimică, Curățare cu presiune înaltă, Curățare caroserie și ornamente (detaliată), Curățare roți (detaliată), Uscare cu prosoape ultra-absorbante (fără urme), Decontaminare mecanică suprafețe vitrate (cu argilă), Decontaminare mecanică caroserie (cu argilă), Corectare lac (3 steps correction), Tratament glaze (extra luciu), Tratament de protecție caroserie (ceară premium), Tratament hidrofobic - suprafețe vitrate, Revitalizare faruri / stopuri, Revitalizare ornamente din plastic / crom / piano black",
+    discount_procent: 15
+  },
+  {
+    nume_pachet: "RNEW",
+    tip: "interior",
+    servicii_incluse: "Aspirare mochetă, Aspirare preșuri, Aspirare scaune și banchetă, Suflare cu aer comprimat a habitaclului, Aspirare portbagaj, Curățare mochetă (detaliată), Curățare preșuri textile (detaliată), Curățare scaune și banchetă - textile (detaliată), Curățare panouri laterale - textile (detaliată), Tratament de hidratare și protejare dedicat suprafețelor din piele, Curățare planșă de bord și consolă centrală (detaliată), Curățare mânere / butoane / ornamente (detaliată), Hidratare și protejare suprafețe din plastic / cauciuc, Curățare compartimente (uși / cotieră / torpedou) (detaliată) , Curățare portbagaj (detaliată), Curățare volan - spuma/plastic, Curățare plafon textil, Curățare suprafețe vitrate, Curățare pedale",
+    discount_procent: 15
+  }
+]
---
+++ servicii_interior.js
@@ -0,0 +1,242 @@
+const data_interior = [
+  {
+    serviciu: "Aspirare mochetă",
+    ore_lucru: "0,5",
+    consumabile: 20,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Aspirare preșuri",
+    ore_lucru: "0,66",
+    consumabile: 18,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Aspirare scaune și banchetă",
+    ore_lucru: "0,5",
+    consumabile: 16,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Aspirare portbagaj",
+    ore_lucru: "0,25",
+    consumabile: 10,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare mochetă (standard)",
+    ore_lucru: "1,5",
+    consumabile: 35,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare mochetă (detaliată)",
+    ore_lucru: "3",
+    consumabile: 55,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare preșuri de cauciuc",
+    ore_lucru: "0,7",
+    consumabile: 20,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare preșuri textile (standard)",
+    ore_lucru: "1",
+    consumabile: 25,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare preșuri textile (detaliată)",
+    ore_lucru: "2",
+    consumabile: 40,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Suflare cu aer comprimat a habitaclului",
+    ore_lucru: "1",
+    consumabile: 30,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare scaune și banchetă - textile (standard)",
+    ore_lucru: "2",
+    consumabile: 60,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare scaune și banchetă - piele (standard)",
+    ore_lucru: "2",
+    consumabile: 75,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare scaune și banchetă - textile (detaliată)",
+    ore_lucru: "4",
+    consumabile: 80,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare scaune și banchetă - piele (detaliată)",
+    ore_lucru: "4",
+    consumabile: 95,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare panouri laterale - textile (standard)",
+    ore_lucru: "1",
+    consumabile: 40,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare panouri laterale - piele (standard)",
+    ore_lucru: "1",
+    consumabile: 50,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare panouri laterale - textile (detaliată)",
+    ore_lucru: "2",
+    consumabile: 80,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare panouri laterale - piele (detaliată)",
+    ore_lucru: "2",
+    consumabile: 100,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Tratament de hidratare și protejare dedicat suprafețelor din piele",
+    ore_lucru: "1,5",
+    consumabile: 120,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare planșă de bord și consolă centrală (standard)",
+    ore_lucru: "0,25",
+    consumabile: 12,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare mânere / butoane / ornamente (standard)",
+    ore_lucru: "0,25",
+    consumabile: 15,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare planșă de bord și consolă centrală (detaliată)",
+    ore_lucru: "1",
+    consumabile: 17,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare mânere / butoane / ornamente (detaliată)",
+    ore_lucru: "1",
+    consumabile: 20,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Hidratare și protejare suprafețe din plastic / cauciuc",
+    ore_lucru: "1",
+    consumabile: 15,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare compartimente (uși / cotieră / torpedou) (standard)",
+    ore_lucru: "0,75",
+    consumabile: 20,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare compartimente (uși / cotieră / torpedou) (detaliată)",
+    ore_lucru: "1,5",
+    consumabile: 30,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare portbagaj (simplă)",
+    ore_lucru: "0,5",
+    consumabile: 37,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare portbagaj (detaliată)",
+    ore_lucru: "1,5",
+    consumabile: 48,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare volan - spuma/plastic",
+    ore_lucru: "0,25",
+    consumabile: 10,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare volan - piele",
+    ore_lucru: "0,5",
+    consumabile: 20,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare plafon textil",
+    ore_lucru: "1",
+    consumabile: 35,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare plafon alcantara",
+    ore_lucru: "2",
+    consumabile: 75,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare centuri de siguranță",
+    ore_lucru: "1",
+    consumabile: 25,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare suprafețe vitrate",
+    ore_lucru: "0,75",
+    consumabile: 16,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare pedale",
+    ore_lucru: "0,25",
+    consumabile: 10,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare păr de animale",
+    ore_lucru: "1,5",
+    consumabile: 15,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare gumă de mestecat",
+    ore_lucru: "0,5",
+    consumabile: 12,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare pată",
+    ore_lucru: "1",
+    consumabile: 18,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Curățare mochetă portbagaj",
+    ore_lucru: "1",
+    consumabile: 25,
+    multiplicabil: "DA"
+  },
+  {
+    serviciu: "Demontare scaune și banchetă",
+    ore_lucru: "1,5",
+    consumabile: 5,
+    multiplicabil: "DA"
+  }
+]

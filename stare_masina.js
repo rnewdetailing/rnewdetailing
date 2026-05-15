ee1a9db6da92fad0e3fb1dc7d05a26b28a9e75eb
rnewdetailing <r.new.detailing@gmail.com>
2026-05-15 11:35:24 +0300

    Add files via upload

---
+++ stare_masina.js
@@ -0,0 +1,14 @@
+ const data_stare = [
+  {
+    stare: "Întrețiută",
+    coef_Stare: 1
+  },
+  {
+    stare: "Exploatată",
+    coef_Stare: "1.2"
+  },
+  {
+    stare: "Neglijată",
+    coef_Stare: "1.5"
+  }
+]
